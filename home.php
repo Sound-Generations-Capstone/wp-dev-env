c32cfd7e30c37297867a018ef733ec494e738f43
benwakaba <101528329+benwakaba@users.noreply.github.com>
2024-11-12 13:13:19 -0800

    added home.php

---
+++ home.php
@@ -0,0 +1,180 @@
+
+//Lower half of the webpage
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Get Involved</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"backgroundColor":"purplelt"} -->
+<div class="wp-block-column has-purplelt-background-color has-background"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<h2 class="wp-block-heading has-text-align-center has-white-color has-text-color has-link-color">Support Us</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Every contribution helps uphold Sound Generations' network of essential services for our aging neighbors in King County.<br><br>Donate today and empower an older adult to enjoy a better quality of life.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"align":"wide","layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons alignwide"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button">Donate</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"backgroundColor":"orangedrk"} -->
+<div class="wp-block-column has-orangedrk-background-color has-background"><!-- wp:heading {"textAlign":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<h2 class="wp-block-heading has-text-align-center has-white-color has-text-color has-link-color">Subscribe</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">Sign up for our monthly newsletter to get the latest news, volunteer opportunities, tips and other ways to get involved with Sound Generations.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center","verticalAlignment":"stretch"}} -->
+<div class="wp-block-group alignwide"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-text-align-center wp-element-button">Subscribe to E-News</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">What's Happening</h2>
+<!-- /wp:heading -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:gallery {"linkTo":"none"} -->
+<figure class="wp-block-gallery has-nested-images columns-default is-cropped"><!-- wp:image {"id":15962,"sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="http://localhost:8882/wp-content/uploads/2024/11/PRIDE_6-2024-1024x683.webp" alt="" class="wp-image-15962"/><figcaption class="wp-element-caption">Celebrating Pride Month: Empowering and Supporting LGBTQ+ Older Adults</figcaption></figure>
+<!-- /wp:image --></figure>
+<!-- /wp:gallery --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:gallery {"linkTo":"none"} -->
+<figure class="wp-block-gallery has-nested-images columns-default is-cropped"><!-- wp:image {"id":15963,"sizeSlug":"large","linkDestination":"none","style":{"color":{"duotone":"unset"}}} -->
+<figure class="wp-block-image size-large"><img src="http://localhost:8882/wp-content/uploads/2024/11/03-2024-womens-history-2-1024x683.webp" alt="" class="wp-image-15963"/><figcaption class="wp-element-caption">Empowering Women: Celebrating Contributions and Dedication</figcaption></figure>
+<!-- /wp:image --></figure>
+<!-- /wp:gallery --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:gallery {"linkTo":"none"} -->
+<figure class="wp-block-gallery has-nested-images columns-default is-cropped"><!-- wp:image {"id":15964,"sizeSlug":"large","linkDestination":"none","style":{"color":{"duotone":"unset"}}} -->
+<figure class="wp-block-image size-large"><img src="http://localhost:8882/wp-content/uploads/2024/11/FebEnews_ActsofKindness-982x655-1.jpg" alt="" class="wp-image-15964"/><figcaption class="wp-element-caption">Month of Random Acts of Kindness: Extend Kindness to Older Neighbors</figcaption></figure>
+<!-- /wp:image --></figure>
+<!-- /wp:gallery --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
+//footer
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:search {"label":"Search","width":430,"widthUnit":"px","buttonText":"Search","buttonUseIcon":true,"align":"right"} /-->
+
+<!-- wp:social-links {"align":"right","layout":{"type":"flex","justifyContent":"right"}} -->
+<ul class="wp-block-social-links alignright"><!-- wp:social-link {"url":"facebook.com","service":"facebook"} /-->
+
+<!-- wp:social-link {"url":"twitter.com","service":"x"} /-->
+
+<!-- wp:social-link {"url":"tumblr.com","service":"tumblr"} /-->
+
+<!-- wp:social-link {"url":"youtube.com","service":"youtube"} /--></ul>
+<!-- /wp:social-links --></div>
+<!-- /wp:group -->
+ <!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:image {"lightbox":{"enabled":false},"id":15957,"width":"224px","height":"auto","sizeSlug":"full","linkDestination":"custom","align":"left"} -->
+<figure class="wp-block-image alignleft size-full is-resized"><a href="http://localhost:8882/?page_id=15956"><img src="http://localhost:8882/wp-content/uploads/2024/11/image.png" alt="" class="wp-image-15957" style="width:224px;height:auto"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><a href="http://localhost:8882/?page_id=29" data-type="page" data-id="29">About</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><a href="http://localhost:8882/?page_id=1103" data-type="page" data-id="1103">Careers</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><a href="http://localhost:8882/?page_id=8833" data-type="page" data-id="8833">News &amp; Events</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><a href="http://localhost:8882/?page_id=15961" data-type="page" data-id="15961">Volunteer</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><a href="http://programs">Our Programs</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<h3 class="wp-block-heading has-white-color has-text-color has-link-color">Resources</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><a href="https://soundgenerations.org/about/leadership-board-of-directors/">Leadership &amp; Board of Directors</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="https://soundgenerations.org/about-us/news-media-center/">News &amp; Media Center</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="https://soundgenerations.org/about-us/terms-of-use/">Terms of Use</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="https://soundgenerations.org/about-us/privacy-policy/">Privacy Policy</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Site Map</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<h3 class="wp-block-heading has-white-color has-text-color has-link-color">Sound Generations</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Downtown Administrative Offices<br>Lillian Rice Building<br>2208 Second Avenue, Suite 100<br>Seattle, WA 98121-2055</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Phone: 206-448-5757<br>Fax: 206-448-5766<br><a href="mailto:info@soundgenerations.org">info@soundgenerations.org</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p>Sound Generations is a 501(c)3 non profit.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Our tax ID number is 91-0823767.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":13417,"linkDestination":"custom"} -->
+<figure class="wp-block-image"><a href="https://www.guidestar.org/profile/91-0823767"><img src="https://soundgenerations.org/wp-content/uploads/MicrosoftTeams-image-5.png" alt="Platinum Transparency 2023" class="wp-image-13417"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
