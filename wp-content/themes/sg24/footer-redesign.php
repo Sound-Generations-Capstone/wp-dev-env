d548fe85ff39b41b819993f332f30d2a8b0b6b50
almccall <amccall@seattleu.edu>
2024-11-13 00:32:38 -0800

    Add footer redesign for prototype

---
+++ wp-content/themes/sg24/footer-redesign.php
@@ -0,0 +1,85 @@
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+<div class="wp-block-group"><!-- wp:search {"label":"","placeholder":"Search...","width":430,"widthUnit":"px","buttonText":"Search","buttonUseIcon":true,"align":"right","style":{"elements":{"link":{"color":{"text":"#00000070"}}},"color":{"text":"#00000070"}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"stretch"} -->
+<div class="wp-block-column is-vertically-aligned-stretch"><!-- wp:image {"lightbox":{"enabled":false},"id":16032,"width":"216px","height":"auto","sizeSlug":"full","linkDestination":"custom"} -->
+<figure class="wp-block-image size-full is-resized"><a href="http://localhost:8882/?page_id=16001"><img src="http://localhost:8882/wp-content/uploads/2024/11/logo-sound-generations-rev2019.png" alt="" class="wp-image-16032" style="width:216px;height:auto"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><a href="http://localhost:8882/?page_id=29" data-type="page" data-id="29">About</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><a href="http://localhost:8882/?page_id=1103" data-type="page" data-id="1103">Careers</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><a href="http://localhost:8882/?page_id=8833" data-type="page" data-id="8833">News &amp; Events</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><a href="http://localhost:8882/?page_id=15961" data-type="page" data-id="15961">Volunteer</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center"><a href="http://programs">Our Programs</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"top"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<h3 class="wp-block-heading has-white-color has-text-color has-link-color">Resources</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><a href="https://soundgenerations.org/about/leadership-board-of-directors/">Leadership &amp; Board of Directors</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="https://soundgenerations.org/about-us/news-media-center/">News &amp; Media Center</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="https://soundgenerations.org/about-us/terms-of-use/">Terms of Use</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="https://soundgenerations.org/about-us/privacy-policy/">Privacy Policy</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Site Map</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<h3 class="wp-block-heading has-white-color has-text-color has-link-color">Sound Generations</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Downtown Administrative Offices<br>Lillian Rice Building<br>2208 Second Avenue, Suite 100<br>Seattle, WA 98121-2055</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Phone: 206-448-5757<br>Fax: 206-448-5766<br><a href="mailto:info@soundgenerations.org">info@soundgenerations.org</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:paragraph -->
+<p>Sound Generations is a 501(c)3 non profit.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Our tax ID number is 91-0823767.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"id":13417,"linkDestination":"custom"} -->
+<figure class="wp-block-image"><a href="https://www.guidestar.org/profile/91-0823767"><img src="https://soundgenerations.org/wp-content/uploads/MicrosoftTeams-image-5.png" alt="Platinum Transparency 2023" class="wp-image-13417"/></a></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
