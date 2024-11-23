b40dce0b02df2f52f05827f16efa1bd351743141
almccall <amccall@seattleu.edu>
2024-11-22 22:51:48 -0800

    Add HTML skeleton for news center page's top half

---
+++ wp-content/themes/sg24/page-news-center.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Template Name: News Center
+ * 
+ * @package Sound_Generations
+ */
+get_header();
+?>
+<img src="" class="banner" />
+<div class="e-news-signup">
+  <h2>Sign up for E-News</h2>
+  <p> 
+    Get the latest news from Sound Generations and stay in the know.
+  </p>
+  <!-- Gravity forms button here-->
+</div>
+<!-- wp:heading {"textAlign":"center","level":1,"style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"textColor":"black"} -->
+<h1 class="wp-block-heading has-text-align-center has-black-color has-text-color"><strong>Sound Generations In The News</strong></h1>
+<!-- /wp:heading -->
