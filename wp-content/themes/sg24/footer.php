26504d9f5ae8350e198e67af738f4690c8972d70
almccall <amccall@seattleu.edu>
2024-11-25 12:41:28 -0800

    Tweak footer spacing and bcolor

--- wp-content/themes/sg24/footer.php
+++ wp-content/themes/sg24/footer.php
@@ -1,54 +1,54 @@
 <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
-<div class="wp-block-group"><!-- wp:search {"label":"","placeholder":"Search...","width":430,"widthUnit":"px","buttonText":"Search","buttonUseIcon":true,"align":"right","style":{"elements":{"link":{"color":{"text":"#00000070"}}},"color":{"text":"#00000070"}}} /--></div>
+<div class="wp-block-group has-dark-grey-background-color"><!-- wp:search {"label":"","placeholder":"Search...","width":430,"widthUnit":"px","buttonText":"Search","buttonUseIcon":true,"align":"right","style":{"elements":{"link":{"color":{"text":"#00000070"}}},"color":{"text":"#00000070"}}} /--></div>
 <!-- /wp:group -->
 
 <!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"verticalAlignment":"stretch"} -->
+<div class="wp-block-columns has-dark-grey-background-color has-white-color footer-padding"><!-- wp:column {"verticalAlignment":"stretch"} -->
   <div class="wp-block-column is-vertically-aligned-stretch"><!-- wp:image {"lightbox":{"enabled":false},"id":16032,"width":"216px","height":"auto","sizeSlug":"full","linkDestination":"custom"} -->
     <figure class="wp-block-image size-full is-resized"><a href="/?page_id=16001"><img src="/wp-content/uploads/2024/11/logo-sound-generations-rev2019.png" alt="" class="wp-image-16032" style="width:216px;height:auto" /></a></figure>
     <!-- /wp:image -->
 
     <!-- wp:paragraph {"align":"center"} -->
-    <p class="has-text-align-center"><a href="/?page_id=29" data-type="page" data-id="29">About</a></p>
+    <p class="has-text-align-center has-white-color"><a href="/?page_id=29" data-type="page" data-id="29" class="has-white-color">About</a></p>
     <!-- /wp:paragraph -->
 
     <!-- wp:paragraph {"align":"center"} -->
-    <p class="has-text-align-center"><a href="/?page_id=1103" data-type="page" data-id="1103">Careers</a></p>
+    <p class="has-text-align-center has-white-color"><a href="/?page_id=1103" data-type="page" data-id="1103" class="has-white-color">Careers</a></p>
     <!-- /wp:paragraph -->
 
     <!-- wp:paragraph {"align":"center"} -->
-    <p class="has-text-align-center"><a href="/?page_id=8833" data-type="page" data-id="8833">News &amp; Events</a></p>
+    <p class="has-text-align-center has-white-color"><a href="/?page_id=8833" data-type="page" data-id="8833" class="has-white-color">News &amp; Events</a></p>
     <!-- /wp:paragraph -->
 
     <!-- wp:paragraph {"align":"center"} -->
-    <p class="has-text-align-center"><a href="/?page_id=15961" data-type="page" data-id="15961">Volunteer</a></p>
+    <p class="has-text-align-center has-white-color"><a href="/?page_id=15961" data-type="page" data-id="15961" class="has-white-color">Volunteer</a></p>
     <!-- /wp:paragraph -->
 
     <!-- wp:paragraph {"align":"center"} -->
-    <p class="has-text-align-center"><a href="http://programs">Our Programs</a></p>
+    <p class="has-text-align-center has-white-color"><a href="http://programs" class="has-white-color">Our Programs</a></p>
     <!-- /wp:paragraph -->
   </div>
   <!-- /wp:column -->
 
   <!-- wp:column {"verticalAlignment":"top"} -->
-  <div class="wp-block-column is-vertically-aligned-top"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+  <div class="wp-block-column is-vertically-aligned-top has-white-color"><!-- wp:heading {"level":3,"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
     <h3 class="wp-block-heading has-white-color has-text-color has-link-color">Resources</h3>
     <!-- /wp:heading -->
 
     <!-- wp:paragraph -->
-    <p><a href="https://soundgenerations.org/about/leadership-board-of-directors/">Leadership &amp; Board of Directors</a></p>
+    <p><a href="https://soundgenerations.org/about/leadership-board-of-directors/" class="has-white-color">Leadership &amp; Board of Directors</a></p>
     <!-- /wp:paragraph -->
 
     <!-- wp:paragraph -->
-    <p><a href="https://soundgenerations.org/about-us/news-media-center/">News &amp; Media Center</a></p>
+    <p><a href="https://soundgenerations.org/about-us/news-media-center/" class="has-white-color">News &amp; Media Center</a></p>
     <!-- /wp:paragraph -->
 
     <!-- wp:paragraph -->
-    <p><a href="https://soundgenerations.org/about-us/terms-of-use/">Terms of Use</a></p>
+    <p><a href="https://soundgenerations.org/about-us/terms-of-use/" class="has-white-color">Terms of Use</a></p>
     <!-- /wp:paragraph -->
 
     <!-- wp:paragraph -->
-    <p><a href="https://soundgenerations.org/about-us/privacy-policy/">Privacy Policy</a></p>
+    <p><a href="https://soundgenerations.org/about-us/privacy-policy/" class="has-white-color">Privacy Policy</a></p>
     <!-- /wp:paragraph -->
 
     <!-- wp:paragraph -->
@@ -73,7 +73,7 @@
   <!-- /wp:column -->
 
   <!-- wp:column -->
-  <div class="wp-block-column"><!-- wp:paragraph -->
+  <div class="wp-block-column has-white-color"><!-- wp:paragraph -->
     <p>Sound Generations is a 501(c)3 non profit.</p>
     <!-- /wp:paragraph -->
 
