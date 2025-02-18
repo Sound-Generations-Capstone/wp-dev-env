9711ee40c2e5dd568ab9745ddaf2893742593b7e
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-02-18 10:09:32 -0800

    tests: example template

--- cypress/e2e/connect.cy.js
+++ cypress/e2e/connect.cy.js
@@ -1,6 +1,8 @@
+/// <reference types="cypress" />
+
 describe("connecting to site works", () => {
   it("with env variable", () => {
-    const url = Cypress.env('SITE_URL');
+    const url = Cypress.env("SITE_URL");
     cy.visit(url);
   });
 });
