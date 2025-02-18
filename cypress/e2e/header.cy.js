0d6919756f6a31163cbbe7a58c512a2e5974a684
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-02-18 10:05:36 -0800

    tests: header tabs are centered, donate button stands out

--- cypress/e2e/header.cy.js
+++ cypress/e2e/header.cy.js
@@ -1,17 +1,35 @@
 /// <reference types="cypress" />
 
-describe('header content', () => {
+describe("Header content", () => {
   beforeEach(() => {
-    const url = Cypress.env('SITE_URL');
-    cy.visit(url)
-  })
+    const url = Cypress.env("SITE_URL");
+    cy.visit(url);
+  });
 
-  specify('', () => {
+  specify("Tab headers are centered", () => {
+    cy.get(".main-navigation ul").should(($ul) => {
+      const rect = $ul[0].getBoundingClientRect();
+      const viewportWidth = Cypress.config("viewportWidth");
 
-  })
+      // Calculate the element's center
+      const elementCenterX = rect.left + rect.width / 2;
 
-  specify('', () => {
-    
-  })
+      // Calculate the viewport center
+      const viewportCenterX = viewportWidth / 2;
 
-})
+      // Allow a small margin for precision
+      const margin = 10;
+
+      // Assert the element is centered horizontally
+      expect(elementCenterX).to.be.closeTo(viewportCenterX, margin);
+    });
+  });
+
+  specify("Donate button stands out", () => {
+    let button = cy.get(
+      "#mega-menu-wrap-primary #mega-menu-primary > li:last-child > a.mega-menu-link"
+    );
+    button.should("have.css", "font-weight", "700");
+    button.should("have.css", "border", "3px solid rgb(253, 130, 4)");
+  });
+});
