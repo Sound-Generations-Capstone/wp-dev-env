32dc3eff39d3a8b0f8ebcb70dc6775674157faa3
erinjinz <123047154+erinjinz@users.noreply.github.com>
2025-03-07 13:01:05 -0800

    program redesign page templates (#16)

---
+++ wp-content/themes/sg24/page-our-programs.php
@@ -0,0 +1,150 @@
+<?php
+
+/**
+ * Template Name: Our Programs
+ * 
+ * @package Sound_Generations
+ */
+
+get_header();
+?>
+
+<!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center"><strong>Our Senior Assistance Programs &amp; Services</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Sound Generations is the largest provider of comprehensive senior assistance services for aging adults and their loved ones within the Greater-Seattle Area and King County. We connect older adults and adults with disabilities to vital resources in their communities by providing food security, transportation, health and wellness, and assistance services.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"overlayColor":"purple","isUserOverlayColor":true,"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-purple-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","style":{"typography":{"fontSize":"28px"}}} -->
+<p class="has-text-align-center" style="font-size:28px"><strong>Food Security</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"width":"120px","height":"120px","scale":"cover","sizeSlug":"large","align":"center"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><img src="https://soundgenerations.org/wp-content/uploads/Food-Security.png" alt="" style="object-fit:cover;width:120px;height:120px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item {"className":".page-id-123 ul li a {     color: white !important; }","style":{"typography":{"textDecoration":"none"}}} -->
+<li class=".page-id-123 ul li a { color: white !important; }" style="text-decoration:none"><a href="https://soundgenerations.org/our-programs/food-security/meals-on-wheels/">Meals on Wheels</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://soundgenerations.org/our-programs/food-security/community-dining/">Community Dining</a></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"overlayColor":"purpledrk","isUserOverlayColor":true,"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-purpledrk-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","style":{"typography":{"fontSize":"28px"}}} -->
+<p class="has-text-align-center" style="font-size:28px"><strong>Transportation</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"width":"120px","height":"120px","scale":"cover","sizeSlug":"large","align":"center"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><img src="https://soundgenerations.org/wp-content/uploads/Transportation.png" alt="" style="object-fit:cover;width:120px;height:120px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item {"className":"ul li a, ol li a {     color: white !important; }"} -->
+<li class="ul li a, ol li a {     color: white !important; }"><a href="https://soundgenerations.org/our-programs/transportation/hyde-shuttle/">Hyde Shuttle</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://soundgenerations.org/our-programs/transportation/volunteer-transportation/">Volunteer Transportation</a></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"overlayColor":"orangedrk","isUserOverlayColor":true,"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-orangedrk-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","style":{"typography":{"fontSize":"28px"}}} -->
+<p class="has-text-align-center" style="font-size:28px"><strong>Health &amp; Wellness</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"width":"120px","height":"120px","scale":"cover","sizeSlug":"large","align":"center"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><img src="https://soundgenerations.org/wp-content/uploads/Health-and-Wellness.png" alt="" style="object-fit:cover;width:120px;height:120px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li><a href="https://soundgenerations.org/health-wellness/fitness/">Fitness</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://soundgenerations.org/health-wellness/fall-prevention/">Fall Prevention</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://soundgenerations.org/health-wellness/health-management/">Health Management</a></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:cover {"overlayColor":"purplelt","isUserOverlayColor":true,"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover"><span aria-hidden="true" class="wp-block-cover__background has-purplelt-background-color has-background-dim-100 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"center","placeholder":"Write title…","style":{"typography":{"fontSize":"28px"}}} -->
+<p class="has-text-align-center" style="font-size:28px"><strong>Assistance Services</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:image {"width":"120px","height":"120px","scale":"cover","sizeSlug":"large","align":"center"} -->
+<figure class="wp-block-image aligncenter size-large is-resized"><img src="https://soundgenerations.org/wp-content/uploads/Assistance-Services.png" alt="" style="object-fit:cover;width:120px;height:120px"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:list -->
+<ul class="wp-block-list"><!-- wp:list-item -->
+<li><a href="https://soundgenerations.org/our-programs/pathways-information-assistance/">Pathways Information &amp; Assistance</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://soundgenerations.org/eldereducationinstitute/">Elder Education Institute</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://soundgenerations.org/our-programs/caregiver-support/">Caregiver Support</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://soundgenerations.org/our-programs/minor-home-repair/">Minor Home Repair</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://soundgenerations.org/our-programs/senior-rights-assistance/">Legal Assistance</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://soundgenerations.org/our-programs/grat/">Crisis Prevention</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://soundgenerations.org/assistance-services/health-related-social-needs/">Health-Related Social Needs</a></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:paragraph -->
+<p>Through our programs, we provide essential services like Meals on Wheels and community dining options, making sure no senior has to worry about where their next meal is coming from.We offer free transportation services for seniors through our Hyde Shuttle and volunteer transportation programs, helping older adults get to medical appointments, grocery stores, or just spend time with friends. Our health and wellness programs, including senior fitness classes and fall prevention workshops, are designed to keep seniors active with a health management system in place.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>We connect seniors in King County to essential&nbsp; resources through Pathways Information &amp; Assistance, offering services like pro bono legal help for seniors, minor home repair, and crisis prevention to keep them safe and secure. Our caregiver support programs, including specialized help for veterans, provide much-needed relief, while the Elder Education Institute shares valuable knowledge with the community. We also offer support groups for caregivers of elderly family members, ensuring they have a supportive community.&nbsp;</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>At Sound Generations, we’re dedicated to making sure older adults in King County feel supported, valued, and empowered.</p>
+<!-- /wp:paragraph -->
+
+<?php
+get_footer();
+?>
