92bfe76f8d83ad8f3905ad24af1416acbe1ed04b
Anthony Ngo <anthony.ngo.dev@gmail.com>
2025-03-07 11:12:38 -0800

    move volunteer page to own file

--- wp-content/themes/sg24/searchform.php
+++ wp-content/themes/sg24/searchform.php
@@ -1,16 +1,22 @@
 <?php
 /* Custom search form */
 ?>
-<form role="search" method="get" id="global-search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+<form role="search" method="get" id="global-search-form" action="<?php echo esc_url(home_url('/')); ?>">
   <div class="input-group">
-    <input 
-      type="search" 
-      class="form-control border-0"
-      placeholder="Search..." 
-      aria-label="search Sound Generations" name="s" 
-      id="search-input" 
-      value="<?php echo get_search_query(); ?>"
-      >
-    </input>
+    <label>
+      <span class="screen-reader-text"><?php echo _x('Search for:', 'label', 'sg20'); ?></span>
+      <input
+        type="search"
+        class="form-control border-0"
+        placeholder="Search..."
+        aria-label="search Sound Generations" name="s"
+        id="search-input"
+        value="<?php echo get_search_query(); ?>">
+      </input>
+    </label>
+    <?php if (!empty($_GET['category_name'])) : ?>
+      <input type="hidden" name="category_name" value="<?php echo esc_attr($_GET['category_name']); ?>">
+    <?php endif; ?>
+    <button type="submit" class="search-submit"><?php echo esc_html_x('Search', 'submit button', 'sg20'); ?></button>
   </div>
 </form>
