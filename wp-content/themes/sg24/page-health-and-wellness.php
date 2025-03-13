ae1edf7462c91e5fb24f44adfe9174b69b76ce70
almccall <amccall@seattleu.edu>
2025-03-13 12:50:13 -0700

    Fix health-wellness fitness link

--- wp-content/themes/sg24/page-health-and-wellness.php
+++ wp-content/themes/sg24/page-health-and-wellness.php
@@ -43,7 +43,7 @@ get_header();
     <!-- /wp:paragraph -->
 
     <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
-    <p class="has-white-color has-text-color has-link-color light-background"><a href="<?echo esc_url(get_permalink(get_page_by_path('health-wellness/fitness/')))?>"><strong>Learn more about EnhanceFitness and other fitness programs.</strong></a></p>
+    <p class="has-white-color has-text-color has-link-color light-background"><a href="<?php echo esc_url(get_permalink(get_page_by_path('health-wellness/fitness/')))?>"><strong>Learn more about EnhanceFitness and other fitness programs.</strong></a></p>
     <!-- /wp:paragraph --></div>
     <!-- /wp:column --></div>
     <!-- /wp:columns --></div></div>
